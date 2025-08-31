1ee92eb4a767efbb2c6f6e865df8fd0bd43ac0fc
Yousif Aldahan <warriorwithinyousif@gmail.com>
2025-08-31 11:22:10 +0000

    landing page is working now but the login page is white

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+
+import { LoginForm } from "@/components/login-form";
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-secondary p-4">
+      <LoginForm />
+    </main>
+  );
+}
